d0a855f3099a5852ec9b21dcb03fb287ef19474c
Nicofisi <m.lomiak@gmail.com>
2016-04-20 14:22:51 +0200

    initial commit

---
+++ src/main/java/pl/pickaxe/sharpy/sharpy/App.java
@@ -0,0 +1,13 @@
+package pl.pickaxe.sharpy.sharpy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
